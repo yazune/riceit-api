be944a9c4fc2b632a483e3e6538e0a9a277f67f8
yazune <grzegorz.infs@gmail.com>
2020-11-27 23:55:06 +0100

    Initial commit

---
+++ src/main/java/com/agh/riceitapi/RiceitApiApplication.java
@@ -0,0 +1,13 @@
+package com.agh.riceitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiceitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiceitApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agh/riceitapi/RiceitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agh.riceitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiceitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
